6ee646279afacd2fea62a9891cf01f23338d6e57
guidohalley <guidohalley@hotmail.com>
2025-10-21 17:33:38 -0300

    refactor(layout): cambiar layouts a modo default

--- frontend/src/components/layouts/DeckedLayout.tsx
+++ frontend/src/components/layouts/DeckedLayout.tsx
@@ -37,7 +37,7 @@ const DeckedLayout = () => {
                         headerEnd={<HeaderActionsEnd />}
                     />
                     <SecondaryHeader contained />
-                    <View pageContainerType="contained" />
+                    <View pageContainerType="default" />
                 </div>
             </div>
         </div>
